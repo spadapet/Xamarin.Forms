17f077fb8f325e988f6c92297e275f8f2ecd1706
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-10-27 11:27:03 +0100

    Propagate BindingContext to GradientBrush

--- Xamarin.Forms.Core/VisualElement.cs
+++ Xamarin.Forms.Core/VisualElement.cs
@@ -821,6 +821,8 @@ namespace Xamarin.Forms
 		protected override void OnBindingContextChanged()
 		{
 			PropagateBindingContextToStateTriggers();
+			PropagateBindingContextToGradientBrush();
+
 			base.OnBindingContextChanged();
 		}
 
@@ -982,6 +984,17 @@ namespace Xamarin.Forms
 				foreach (var state in group.States)
 					foreach (var stateTrigger in state.StateTriggers)
 						SetInheritedBindingContext(stateTrigger, BindingContext);
+		}
+
+		void PropagateBindingContextToGradientBrush()
+		{
+			var brush = (Brush)GetValue(BackgroundProperty);
+
+			if (brush != null && brush is GradientBrush gradientBrush)
+			{
+				foreach (var item in gradientBrush.GradientStops)
+					SetInheritedBindingContext(item, BindingContext);
+			}
 		}
 
 		void OnFocused() => Focused?.Invoke(this, new FocusEventArgs(this, true));
